048b5b91a540abeed039092fcedf6f90b02d0abb
Ramki29 <ramkie7010@gmail.com>
2023-06-14 16:32:26 +0530

    add square-star-pattern

---
+++ main.js
@@ -0,0 +1,21 @@
+let n= 5;
+let a= "";
+for(let i = 0; i < n; i++) { 
+  for(let j = 0; j < n; j++) { 
+    a+= "*";
+  }
+  a+= "\n";
+}
+console.log(a);	
+
+
+
+
+
+var n=5	
+for(let i=0;i<n;i++){
+	for(j=0;j<n;j++){
+		document.write("*");
+		}
+	document.write("<br>");
+	}	
